3940d1aaad50e43b805925f4dbdf0355723c9779
satyam tiwari <satyam432st@gmail.com>
2023-06-14 22:49:01 +0530

    Time: 8 ms (96.83%), Space: 48.4 MB (7.39%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,51 +1,47 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-       int[][] answer;
-        List<int[]> tempAnswer = new ArrayList<>();
-        //10000 from the problem's constraint
-        int[] intervalEdges = new int[10000];
-        int maxStartOfInterval = 0;
-        for (int i = 0; i < intervals.length; i++) {
-            int startOfInterval = intervals[i][0];
-            int endOfInterval = intervals[i][1];
-            // use Math.max for case [2,3][2,2][3,4] when you don't want override [2,3] with shorter [2,2].
-            // add +1 for cases like this [0,0][1,1][2,2][3,3] and so far and so on.
-            intervalEdges[startOfInterval] = Math.max(intervalEdges[startOfInterval], endOfInterval + 1);
-        }
-        int[] tempInterval = new int[]{-1, -1};
-        for (int startOfCurrentInterval = 0; startOfCurrentInterval < intervalEdges.length; startOfCurrentInterval++) {
-            int endOfTempInterval = tempInterval[1];
-            int endOfCurrentInterval = intervalEdges[startOfCurrentInterval];
-            if (endOfCurrentInterval != 0) {
-                if (endOfTempInterval != -1) {
-                    if (startOfCurrentInterval <= endOfTempInterval) {
-                        // don't forget to do -1 since we've done +1 in the first loop
-                        endOfTempInterval = Math.max(endOfTempInterval, endOfCurrentInterval - 1);
-                        tempInterval[1] = endOfTempInterval;
-                    } else {
-                        tempAnswer.add(tempInterval);
-                        // don't forget to do -1 since we've done +1 in the first loop
-                        tempInterval = new int[]{
-                                startOfCurrentInterval,
-                                endOfCurrentInterval - 1
-                        };
+            int[][] array = new int[10001][];
+
+        // This will go down by 1 whenever we merge intervals.
+        int numIntervals = intervals.length;
+
+        for (int[] interval : intervals) {
+            int[] lastMerged = null;
+            for (int i = interval[0]; i <= interval[1]; i++) {
+                // Check if this i is part of a prior interval.
+                if (array[i] != null) {
+                    // If so, we need to merge intervals.
+                    if (lastMerged != array[i]) {
+                        numIntervals--;
+                        lastMerged = array[i];
+    
+                        // Check if the prior interval started before this one.
+                        if (array[i][0] < i) {
+                            // If so, update those pointers to this new interval.
+                            for (int j = array[i][0]; j < interval[0]; j++) {
+                                array[j] = interval;
+                            }
+                            // Update this interval's lower bound.
+                            interval[0] = array[i][0];
+                        }
+                        // Update this interval's higher bound if necessary.
+                        interval[1] = Math.max(interval[1], array[i][1]);
                     }
-                } else {
-                    tempInterval[0] = startOfCurrentInterval;
-                    // don't forget to do -1 since we've done +1 in the first loop
-                    tempInterval[1] = endOfCurrentInterval - 1;
                 }
+                // Indicate that i is in this (possibly updated) interval.
+                array[i] = interval;
             }
         }
-        int endOfTempInterval = tempInterval[1];
-        if (endOfTempInterval != -1) {
-            tempAnswer.add(tempInterval);
-        }
-        answer = new int[tempAnswer.size()][];
-        for (int i = 0; i < tempAnswer.size(); i++) {
-            answer[i] = tempAnswer.get(i);
-        }
-        return answer;
- 
+        
+        // Put the final intervals into an array to return.
+        int[][] results = new int[numIntervals][];
+        int[] lastInterval = null;
+        for (int i = 0, j = 0; j < numIntervals; i++) {
+            if (array[i] != null && array[i] != lastInterval) {
+                lastInterval = array[i];
+                results[j++] = lastInterval;
+            }
+        }  
+        return results;
     }
 }
